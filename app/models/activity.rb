d39e042cdcc1089af03c042d99dff41d258b5385
Jon1213 <jon1213@gmail.com>
2014-11-20 17:29:27 -0800

    Added the method that will get the activities for a user when they put in a time.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,4 +4,21 @@ class Activity < ActiveRecord::Base
   has_many :favorites
   has_many :favorited, through: :favorites, source: :user
   # has_many :taco, through: :join_table, source: :model_trying_to_return
+
+  def self.fun_activities_with_user time, user=nil
+  	if user
+  		return fun_activities(time).where("id not in (?)", user.favorited.select("activities.id"))
+  	end
+  		
+  	fun_activities time
+  end
+
+
+
+
+  private
+
+  def self.fun_activities time
+  	 where('time_min <= #{time}').where("time_max IS NULL OR time_max >= #{time}")
+  end
 end
